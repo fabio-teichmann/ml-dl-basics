72ba70b253d92dee5bebf7e9e9ef32901f956816
Fabio Teichmann <ft@blocksize-capital.com>
2022-08-14 12:46:33 +0300

    modules

---
+++ python/basics/modules.py
@@ -0,0 +1,10 @@
+# Modules help organize code and programs. Each *.py file is its own module.
+# Modules should be logical groups of functionalities.
+
+# build a few utility functions:
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
