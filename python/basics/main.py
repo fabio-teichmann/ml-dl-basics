0143678f964b1de2ed38b4539c2b325196ff3bea
Fabio Teichmann <ft@blocksize-capital.com>
2022-09-30 08:36:02 +0300

    i/o

--- python/basics/main.py
+++ python/basics/main.py
@@ -2,10 +2,17 @@
 import modules
 import shopping.shopping_cart  # access module within package
 
+
 # all imports will be cached in __pycache__ for use
 
 # print(modules)
 # use functions from different files
-print(modules.divide(10, 2))
-print(modules.multiply(2, 10))
-print(shopping.shopping_cart.buy('apple'))
+# print(modules.divide(10, 2))
+# print(modules.multiply(2, 10))
+# print(shopping.shopping_cart.buy('apple'))
+
+def do_stuff(num):
+    try:
+        return num + 5
+    except ValueError as err:
+        return err
---
+++ python/basics/test.py
@@ -0,0 +1,18 @@
+# File 'test.py' usually used for testing purposes
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 10)
+
+    def test_do_stuff2(self):
+        test_param = 'abc'
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+
+
+unittest.main()
